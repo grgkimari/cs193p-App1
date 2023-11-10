7628bbe1c1c5336281485469aa9ab5c1c577e0a6
grgkimari <grgkimari@gmail.com>
2023-11-11 00:44:04 +0300

    Initial Commit

---
+++ cs193p-App1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cs193p-App1
+//
+//  Created by George on 11/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cs193p-App1/cs193p_App1App.swift
@@ -0,0 +1,17 @@
+//
+//  cs193p_App1App.swift
+//  cs193p-App1
+//
+//  Created by George on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct cs193p_App1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
